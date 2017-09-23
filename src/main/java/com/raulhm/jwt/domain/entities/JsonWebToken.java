d1866e6c0701bc6b556b9c5cc815af26b987bb9b
Raul Herranz Muñoz <hmraul@gmail.com>
2017-09-23 19:06:48 +0200

    Adding empty parts to JWT

--- src/main/java/com/raulhm/jwt/domain/entities/JsonWebToken.java
+++ src/main/java/com/raulhm/jwt/domain/entities/JsonWebToken.java
@@ -1,4 +1,29 @@
 package com.raulhm.jwt.domain.entities;
 
+import com.raulhm.jwt.domain.entities.jwtparts.Header;
+import com.raulhm.jwt.domain.entities.jwtparts.Payload;
+import com.raulhm.jwt.domain.entities.jwtparts.Signature;
+
 public class JsonWebToken {
+  private Header header;
+  private Payload payLoad;
+  private Signature signature;
+
+  public JsonWebToken(Header header, Payload payload) {
+    this.header = header;
+    this.payLoad = payload;
+    this.signature = new Signature();
+  }
+
+  public Header getHeader() {
+    return header;
+  }
+
+  public Payload getPayLoad() {
+    return payLoad;
+  }
+
+  public Signature getSignature() {
+    return signature;
+  }
 }
--- src/test/java/com/raulhm/jwt/domain/entities/JsonWebTokenTest.java
+++ src/test/java/com/raulhm/jwt/domain/entities/JsonWebTokenTest.java
@@ -1,13 +1,34 @@
 package com.raulhm.jwt.domain.entities;
 
 import com.raulhm.jwt.domain.entities.jwtparts.Header;
+import com.raulhm.jwt.domain.entities.jwtparts.Payload;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.assertNotNull;
 
 public class JsonWebTokenTest {
 
+  private static JsonWebToken token;
+
+  @BeforeClass
+  public static void setUp() {
+    token = new JsonWebToken(
+        new Header("JWT", "HS256"),
+        new Payload());
+  }
+
   @Test
   public void hasAHeaderDefined() {
-    assertNotNull(new Header("", ""));
+    assertNotNull(token.getHeader());
+  }
+
+  @Test
+  public void hasAPayLoadDefined() {
+    assertNotNull(token.getPayLoad());
+  }
+
+  @Test
+  public void hasASignatureDefined() {
+    assertNotNull(token.getSignature());
   }
 }
